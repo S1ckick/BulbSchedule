34b9269648d01bf9b85c201b273a7345beca261b
Maksim Subbotin <maksim@entroforce.ru>
2023-05-22 14:28:17 +0300

    full_schedule to file output

--- test/main.cpp
+++ test/main.cpp
@@ -13,16 +13,16 @@ using namespace date;
 int main()
 {
     std::unordered_map<SatName, Satellite> sats;
-    const char russia_location[] = "../DATA_Files/Russia2Constellation/";
+    const char russia_location[] = "../../DATA_Files/Russia2Constellation/";
     int res_parse_russia = parse_russia_to_satellites(russia_location, sats);
 
     std::unordered_map<std::string, Observatory> obs;
-    const char facility_location[] = "../DATA_Files/Facility2Constellation/";
+    const char facility_location[] = "../../DATA_Files/Facility2Constellation/";
     int res_parse_obs = parse_observatory(facility_location, obs, sats);
     algos::build_schedule(sats);
     
     std::ofstream out("sats.txt", std::ofstream::out);
-
+    std::ofstream out_schedule("sats_schedule.txt", std::ofstream::out);
 
     std::istringstream start_date("1/Jun/2027 00:00:00.000");
     std::chrono::system_clock::time_point tp_start;
@@ -37,6 +37,18 @@ int main()
                        std::chrono::milliseconds::period::den).count()
                 << '\n';
         }
+
+        for (auto &interval : item.second.full_schedule){
+            out_schedule << std::fixed << interval->sat_name << " "
+                << (std::chrono::duration<double, std::milli>(interval->start - tp_start) * std::chrono::milliseconds::period::num /
+                       std::chrono::milliseconds::period::den).count()
+                << " " << (std::chrono::duration<double, std::milli>(interval->end - tp_start) * std::chrono::milliseconds::period::num /
+                       std::chrono::milliseconds::period::den).count()
+                << " " << interval->state
+                << " " << interval->capacity_change
+                << '\n';
+        }
+
     }
 
     out.close();
