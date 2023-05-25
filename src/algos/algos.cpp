f3f187ff56e162eb2019d1839c263a097e79bc96
alexundertable <sankek@LAPTOP-4PR8H3AF.localdomain>
2023-05-25 22:30:33 +0300

    fix great plan

--- src/algos/algos.cpp
+++ src/algos/algos.cpp
@@ -16,20 +16,24 @@ Schedule algos::great_plan(const Satellites &sats) {
 	std::vector<Event> events_grid; // vector bcs timepoints are not unique :(
 
 	for (auto &sat: sats) {
-		events_grid.reserve(sat.second.ints_in_area.size());
+		events_grid.reserve(events_grid.size() + sat.second.ints_in_area.size());
 		for (auto &int_area: sat.second.ints_in_area) {
 			events_grid.push_back({int_area->start, int_area->info[0], 1});
 			events_grid.push_back({int_area->end, int_area->info[0], -1});
 		}
 
-		events_grid.reserve(sat.second.ints_observatories.size());
+		events_grid.reserve(events_grid.size() + sat.second.ints_observatories.size());
 		for (auto &int_sat: sat.second.ints_observatories) {
 			events_grid.push_back({int_sat->start, int_sat->info[0], 1});
 			events_grid.push_back({int_sat->end, int_sat->info[0], -1});
 		}
 	}
 
-	auto tp_cmp = [](const Event &a, const Event &b) { return a.tp < b.tp; };
+	auto tp_cmp = [](const Event &a, const Event &b) {
+		if (a.tp == b.tp)
+			return a.direction < b.direction; // handle end points before start points
+		return a.tp < b.tp; 
+	};
 	std::sort(events_grid.begin(), events_grid.end(), tp_cmp);
 
 	std::vector<std::shared_ptr<IntervalInfo>> info_stack;
@@ -47,12 +51,15 @@ Schedule algos::great_plan(const Satellites &sats) {
 		if (event_dir == 1) { // start
 			info_stack.push_back(event_info);
 			if (info_stack.size() > 1) {
-				Interval i(last_trigger, event_tp, info_stack);
-				great_plan.insert(std::make_shared<Interval>(i));
+				if (last_trigger != event_tp) {
+					Interval i(last_trigger, event_tp, info_stack);
+					great_plan.insert(std::make_shared<Interval>(i));
+				}
 			}
 		}
 		else { // end
-			great_plan.insert(std::make_shared<Interval>(Interval(last_trigger, event_tp, info_stack)));
+			if (last_trigger != event_tp)
+				great_plan.insert(std::make_shared<Interval>(Interval(last_trigger, event_tp, info_stack)));
 			info_stack.erase(std::find(info_stack.begin(), info_stack.end(), event.info));
 		}
 
